47f018c3332c1d1a0f1bb1f046b316a592b9e299
Mark Emlyn David Thomas <markt@apache.org>
2009-08-07 21:01:31 +0000

    Move the build and connector tags
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc5.5.x/tags/TOMCAT_5_5_15@802189 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/jk/native/common/jk_context.h
@@ -0,0 +1,137 @@
+/*
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/***************************************************************************
+ * Description: Context Stuff (Autoconf)                                   *
+ * Author:      Henri Gomez <hgomez@apache.org>                            *
+ * Version:     $Revision$                                           *
+ ***************************************************************************/
+#ifndef JK_CONTEXT_H
+#define JK_CONTEXT_H
+
+#include "jk_pool.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif                          /* __cplusplus */
+
+#define CBASE_INC_SIZE   (8)    /* Allocate memory by step of 8 URIs : ie 8 URI by context */
+#define URI_INC_SIZE (8)        /* Allocate memory by step of 8 CONTEXTs : ie 8 contexts by worker */
+
+typedef struct
+{
+
+    /*
+     * Context base (ie examples) 
+     */
+
+    char *cbase;
+
+    /*
+     * Status (Up/Down)
+     */
+
+    int status;
+
+    /*
+     * Num of URI handled 
+     */
+
+    int size;
+
+    /*
+     * Capacity
+     */
+
+    int capacity;
+
+    /*
+     * URL/URIs (autoconf)
+     */
+
+    char **uris;
+}
+jk_context_item_t;
+
+
+typedef struct
+{
+
+    /*
+     * Memory Pool
+     */
+
+    jk_pool_t p;
+    jk_pool_atom_t buf[SMALL_POOL_SIZE];
+
+    /*
+     * Virtual Server (if use)
+     */
+
+    char *virt;
+
+    /*
+     * Num of context handled (ie: examples, admin...)
+     */
+
+    int size;
+
+    /*
+     * Capacity
+     */
+
+    int capacity;
+
+    /*
+     * Context list, context / URIs
+     */
+
+    jk_context_item_t **contexts;
+}
+jk_context_t;
+
+
+/*
+ * functions defined here 
+ */
+
+int context_set_virtual(jk_context_t *c, char *virt);
+
+int context_open(jk_context_t *c, char *virt);
+
+int context_close(jk_context_t *c);
+
+int context_alloc(jk_context_t **c, char *virt);
+
+int context_free(jk_context_t **c);
+
+jk_context_item_t *context_find_base(jk_context_t *c, char *cbase);
+
+char *context_item_find_uri(jk_context_item_t *ci, char *uri);
+
+void context_dump_uris(jk_context_t *c, char *cbase, FILE * f);
+
+jk_context_item_t *context_add_base(jk_context_t *c, char *cbase);
+
+int context_add_uri(jk_context_t *c, char *cbase, char *uri);
+
+
+#ifdef __cplusplus
+}
+#endif                          /* __cplusplus */
+
+#endif                          /* JK_CONTEXT_H */
---
+++ connectors/jk/native/common/jk_lb_worker.h
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/***************************************************************************
+ * Description: load balance worker header file                            *
+ * Author:      Gal Shachor <shachor@il.ibm.com>                           *
+ * Version:     $Revision$                                           *
+ ***************************************************************************/
+
+#ifndef JK_LB_WORKER_H
+#define JK_LB_WORKER_H
+
+#include "jk_logger.h"
+#include "jk_service.h"
+#include "jk_mt.h"
+#include "jk_shm.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif                          /* __cplusplus */
+
+#define JK_LB_WORKER_NAME     ("lb")
+#define JK_LB_WORKER_TYPE     (5)
+#define JK_LB_DEF_DOMAIN_NAME ("unknown")
+
+#define JK_LB_BYREQUESTS      (0)
+#define JK_LB_BYTRAFFIC       (1)
+#define JK_LB_METHOD_REQUESTS ("Request")
+#define JK_LB_METHOD_TRAFFIC  ("Traffic")
+#define JK_LB_LOCK_DEFAULT     (0)
+#define JK_LB_LOCK_PESSIMISTIC (1)
+#define JK_LB_LM_DEFAULT       ("Optimistic")
+#define JK_LB_LM_PESSIMISTIC   ("Pessimistic")
+
+#define WAIT_BEFORE_RECOVER   (60)
+
+struct worker_record
+{
+    jk_worker_t     *w;
+    /* Shared memory worker data */
+    jk_shm_worker_t  *s;
+    /* Current jvmRoute. Can be name or domain */
+    const char       *r;
+};
+typedef struct worker_record worker_record_t;
+
+struct lb_worker
+{
+    worker_record_t *lb_workers;
+    unsigned int num_of_workers;
+    int          lbmethod;
+    int          lblock;
+
+    jk_pool_t p;
+    jk_pool_atom_t buf[TINY_POOL_SIZE];
+
+    jk_worker_t worker;
+    JK_CRIT_SEC cs; 
+
+    /* Shared memory worker data */
+    jk_shm_worker_t  *s;
+};
+typedef struct lb_worker lb_worker_t;
+
+int JK_METHOD lb_worker_factory(jk_worker_t **w,
+                                const char *name, jk_logger_t *l);
+
+#ifdef __cplusplus
+}
+#endif                          /* __cplusplus */
+#endif                          /* JK_LB_WORKER_H */
