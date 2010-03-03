18b393950a75d2e20314946c98bcc18120148557
Konstantin Kolinko <kkolinko@apache.org>
2010-03-03 16:50:15 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48516 Fix possible NPE when user does not exist in directory Patch provided by Kevin Conaway
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc5.5.x/trunk@918564 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
+++ container/catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
@@ -1576,8 +1576,12 @@ public class JNDIRealm extends RealmBase {
         
         User user = getUser(context, username);
         
-        return new GenericPrincipal(this, user.username, user.password ,
-                getRoles(context, user));
+        if (user != null) {
+            return new GenericPrincipal(this, user.username, user.password,
+                    getRoles(context, user));
+        }
+
+        return null;
     }
 
     /**
