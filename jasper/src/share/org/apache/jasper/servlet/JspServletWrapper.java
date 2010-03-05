c8cedbdce41aa4c5a26903bb330a4fe03b62f36e
Mark Emlyn David Thomas <markt@apache.org>
2010-03-05 12:59:52 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48582 Avoid NPE on background compile
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc5.5.x/trunk@919397 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/src/share/org/apache/jasper/servlet/JspServletWrapper.java
+++ jasper/src/share/org/apache/jasper/servlet/JspServletWrapper.java
@@ -157,7 +157,7 @@ public class JspServletWrapper {
     }
 
     public ServletContext getServletContext() {
-        return config.getServletContext();
+        return ctxt.getServletContext();
     }
 
     /**
