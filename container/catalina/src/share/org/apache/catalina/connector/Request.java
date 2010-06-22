03385f32267946c0a67b21faa70a0c47d04dfc8f
Mark Emlyn David Thomas <markt@apache.org>
2010-06-22 11:53:26 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49424 Avoid NPE if client provides no data with a chunked POST request
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc5.5.x/trunk@956856 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/connector/Request.java
+++ container/catalina/src/share/org/apache/catalina/connector/Request.java
@@ -2461,7 +2461,9 @@ public class Request
                 }
                 return;
             }
-            parameters.processParameters(formData, 0, formData.length);
+            if (formData != null) {
+                parameters.processParameters(formData, 0, formData.length);
+            }
         }
 
     }
@@ -2506,6 +2508,9 @@ public class Request
             if (len > 0) {
                 body.append(buffer, 0, len);
             }
+        }
+        if (body.getLength() == 0) {
+            return null;
         }
         if (body.getLength() < body.getBuffer().length) {
             int length = body.getLength();
