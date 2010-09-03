783ddfd52ba36c220ebef2e319dbe8079d5be309
Jim Jagielski <jim@apache.org>
2010-09-03 18:42:48 +0000

    Build is 1.4.2... use ugly method
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc5.5.x/trunk@992411 13f79535-47bb-0310-9956-ffa450edef68

--- container/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/connector/SaveConnectorAction.java
+++ container/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/connector/SaveConnectorAction.java
@@ -490,7 +490,7 @@ public final class SaveConnectorAction extends Action {
                     } catch (NumberFormatException e) {
                         mBServer.setAttribute(coname,
                                 new Attribute("SSLVerifyDepthText",
-                                        Integer.valueOf(10).intValue()));
+                                        new Integer(10)));
                     }
                 
                 String sSLCACertificateFile = cform.getSSLCACertificateFile();
